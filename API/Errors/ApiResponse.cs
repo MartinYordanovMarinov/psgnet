a31eef3894d2252a25b50e281c66cd461762c5ec
MartinYordanovMarinov <martinmarinov214@gmail.com>
2021-12-03 15:08:43 +0200

    End of fifth part

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        
+
+        public int StatusCode {get; set;} 
+
+        public string Message {get; set;}
+
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch{
+
+                400 => "You have made a bad request",
+                401 => " you are not Authorized",
+                404 => "Resource was not found",
+                500 => "Many Errors",
+                _=> null
+            };
+        }
+    }
+}
