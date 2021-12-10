703ad1c9f23c9d46e66a1dbf47c7e3124ae40da3
MartinYordanovMarinov <martinmarinov214@gmail.com>
2021-12-10 22:18:57 +0200

    end of part 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
+import { IProduct } from './shared/models/product';
+import { IPagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +11,9 @@ import { IPagination } from './models/pagination';
 export class AppComponent implements OnInit{
   
   title = 'client';
-  products!: IProduct[];
-  constructor(private http: HttpClient) { }
+  
+  constructor() { }
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response:  any) => {
-      this.products = response.data;
-    }, error =>{
-      console.log(error);
-      
-    });
+    
   }
 }
